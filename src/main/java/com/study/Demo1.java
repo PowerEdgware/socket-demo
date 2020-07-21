8fe87c85270b6355aa09551a2e4fd47b4ce07677
all <all@DESKTOP-56U6K8U>
2020-07-21 10:59:35 +0800

    icmp query

--- src/main/java/com/study/Demo1.java
+++ src/main/java/com/study/Demo1.java
@@ -1,11 +1,17 @@
 package com.study;
 
+import java.net.Inet4Address;
 import java.net.InetAddress;
 
 public class Demo1 {
 
 	public static void main(String[] args) throws Exception {
 //		System.out.println(java.net.InetAddress.getLocalHost().getCanonicalHostName());
-		System.out.println(InetAddress.getByName("allger-1").getHostAddress());
+		//System.out.println(InetAddress.getByName("allger-1").getHostAddress());
+		String host = "139.196.85.101";
+		InetAddress address=Inet4Address.getByName(host);
+		//send ICPM查询消息
+		boolean reach=address.isReachable(30*1000);
+		System.out.println(reach);
 	}
 }
